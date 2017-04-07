62c971dab5254d0c6ed87f1dc171270bc3908a1f
Chris Adams <chris@improbable.org>
2017-04-07 16:44:54 -0400

    Update setup packaging
    * Explicit dependency on `requests[security]`
    * Add dependency on `edgegrid-python`
    * Simplify description in setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name='django-akamai',
-    version='2.0.0pre1',
-    description='A Django app for performing Akamai CCUAPI purge requests',
+    version='2.0.0.dev3',
+    description='A Django app for performing Akamai purge requests',
     author='Ben Boyd',
     author_email='beathan@gmail.com',
     long_description=open('README.rst', 'r').read(),
     url='https://github.com/beathan/django-akamai',
     packages=['django_akamai'],
-    requires=[
+    install_requires=[
         'requests[security]',
         'edgegrid-python',
     ],
